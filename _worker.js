e50a1dfc40bbe1dfb8cfe3c8f033b3ad405375fd
erikaagustiann <157383243+erikaagustiann@users.noreply.github.com>
2025-03-19 07:22:19 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 import { connect } from "cloudflare:sockets";
 
 // Variables
-const rootDomain = "authertwebid.workers.dev"; // Ganti dengan domain utama kalian
+const rootDomain = "authert.web.id"; // Ganti dengan domain utama kalian
 const serviceName = "why"; // Ganti dengan nama workers kalian
 const apiKey = ""; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
 const apiEmail = ""; // Ganti dengan email yang kalian gunakan
